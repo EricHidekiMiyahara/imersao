d78463956c1c6a89cee0525b4ba2981dfdf41c26
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-09-04 09:34:38 -0300

    Create dados.js

---
+++ dados.js
@@ -0,0 +1,10 @@
+let dados = [
+    {
+    titulo : "Mirage",
+    descricao : "Mirage, uma opção favorita entre os jogadores, possui um campo de batalha bem equilibrado e adaptável que acomoda tanto entusiastas casuais como competidores experientes. O layout distinto do mapa e os marcadores estratégicos requerem um sólido domínio da mecânica do jogo, tornando-o uma escolha adorada para quem procura um desafio emocionante."
+    },
+    {
+    titulo : "Vertigo",
+    descricao : "Vertigo é uma excelente escolha para jogadores que querem ultrapassar os seus limites. Ele fornece um ambiente único onde eles podem testar e refinar suas habilidades.", 
+    }
+    ]
