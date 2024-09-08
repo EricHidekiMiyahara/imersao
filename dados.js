15baeda228bd05c161f5d3b5e465de8dbda38709
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-09-08 15:08:15 -0300

    Update index.js

--- dados.js
+++ dados.js
@@ -1,7 +1,7 @@
 let dados = [
     {
-    titulo : "Mirage",
-    descricao : "Mirage, uma opção favorita entre os jogadores, possui um campo de batalha bem equilibrado e adaptável que acomoda tanto entusiastas casuais como competidores experientes. O layout distinto do mapa e os marcadores estratégicos requerem um sólido domínio da mecânica do jogo, tornando-o uma escolha adorada para quem procura um desafio emocionante.",
+    titulo : "Miragem",
+    descricao : "Miragem, uma opção favorita entre os jogadores, possui um campo de batalha bem equilibrado e adaptável que acomoda tanto entusiastas casuais como competidores experientes. O layout distinto do mapa e os marcadores estratégicos requerem um sólido domínio da mecânica do jogo, tornando-o uma escolha adorada para quem procura um desafio emocionante.",
     tags: "favorita favorito melhor"
     },
     {
@@ -10,8 +10,8 @@ let dados = [
     tags: "predio"
     },
     {
-        titulo : "Nuke",
-        descricao : "A jogabilidade de Nuke representa um desafio, tornando-o uma opção excecional para os jogadores que procuram melhorar as suas capacidades e ser mais espertos que os adversários..", 
-        tags: "container" 
+    titulo : "Nuke",
+    descricao : "A jogabilidade de Nuke representa um desafio, tornando-o uma opção excecional para os jogadores que procuram melhorar as suas capacidades e ser mais espertos que os adversários..", 
+    tags: "container" 
     },
     ];
--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+// Função para retornar ao estado inicial da página
+function voltarHome() {
+
+    // Limpa a seção de resultados
+    document.getElementById("resultados-pesquisa").innerHTML = `
+        <div id="sobre" class="sobre-container">
+            <h2>Sobre</h2>
+            <p>Este site foi criado com o objetivo de fornecer informações detalhadas sobre livros, incluindo
+                sinopses e avaliações. Aqui, você pode pesquisar seus livros favoritos e conhecer novas
+                histórias!</p>
+            <p>Alguns dos livros que você encontrará aqui são as principais fontes para filmes e séries populares.
+                Portanto, além de explorar os livros, você também pode descobrir as origens de suas adaptações
+                audiovisuais favoritas.</p>
+            <p>Basta realizar a pesquisa pelo nome do livro que procura. A busca pode ser realizada por
+                palavras-chave, mas é sempre recomendado digitar o nome corretamente para obter os melhores
+                resultados. Caso não encontre o livro em nossa base de dados, nos envie um e-mail para que possamos
+                ajustar a página e incluir esses dados.</p>
+        </div>
+    `;
+}
+
 function pesquisar() {
-    let section = document.getElementById("mapas"); console.log(section);
+    let section = document.getElementById("resultados-pesquisa"); console.log(section);
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
