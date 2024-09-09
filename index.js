a40d27f16d74183dbefb9bea1eedf2a549517950
Eric Miyahara <erichidekimiyahara@gmail.com>
2024-09-08 21:32:28 -0300

    Update styles.css

--- index.js
+++ index.js
@@ -5,18 +5,15 @@ function voltarHome() {
     document.getElementById("resultados-pesquisa").innerHTML = `
         <div id="sobre" class="sobre-container">
             <h2>Sobre</h2>
-            <p>Este site foi criado com o objetivo de fornecer informações detalhadas sobre livros, incluindo
-                sinopses e avaliações. Aqui, você pode pesquisar seus livros favoritos e conhecer novas
-                histórias!</p>
-            <p>Alguns dos livros que você encontrará aqui são as principais fontes para filmes e séries populares.
-                Portanto, além de explorar os livros, você também pode descobrir as origens de suas adaptações
-                audiovisuais favoritas.</p>
-            <p>Basta realizar a pesquisa pelo nome do livro que procura. A busca pode ser realizada por
-                palavras-chave, mas é sempre recomendado digitar o nome corretamente para obter os melhores
-                resultados. Caso não encontre o livro em nossa base de dados, nos envie um e-mail para que possamos
-                ajustar a página e incluir esses dados.</p>
+            <p>Este site foi criado com o objetivo de fornecer informações resumidas sobre os mapas do CS2. Aqui, você pode pesquisar seus mapas favoritos e conhecer mais sobre eles!</p>
+            <p>Basta realizar a pesquisa pelo nome do mapa que procura. A busca pode ser realizada por
+            palavras-chave, mas é sempre recomendado digitar o nome corretamente para obter os melhores
+            resultados. Caso não encontre o mapa em minha base de dados, me envie um e-mail para que possamos
+            ajustar a página e incluir esses dados.</p>
         </div>
     `;
+    // Adiciona a classe "sobre-ativo" à seção "Sobre"
+    document.getElementById("sobre").classList.add("sobre-ativo");
 }
 
 function pesquisar() {
@@ -53,5 +50,14 @@ function pesquisar() {
     }
 
     section.innerHTML = resultados;
+    
+   // Adiciona a classe "card-resultado" a todos os cards e força o navegador a recalcular os estilos
+  const cards = document.querySelectorAll(".card");
+  cards.forEach(card => {
+    card.classList.add("card-resultado");
+    requestAnimationFrame(() => {
+      card.style.backgroundColor = getComputedStyle(card).backgroundColor;
+    });
+  });
 }
 
